b30b88b91502942193a001fd83b772a698aeba18
Hide21K <kazuhide_nii1975@yahoo.co.jp>
2025-07-31 14:20:20 +0900

    UI update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="googlebot" content="noindex, nofollow" />
+        <meta name="bingbot" content="noindex, nofollow" />
+      </Head>
       <body>
         <Main />
         <NextScript />
